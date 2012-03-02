80ef03cb7f53529dbc0febd4efaaa21dbb99af29
Martin Eriksson <martin@mindtheproduct.com>
2012-03-02 16:04:52 +0000

    adding files

---
+++ includes/nav-script.inc.php
@@ -0,0 +1,149 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+<script src="js/jquery.scrollparallax.js" type="text/javascript"></script>
+<script src="js/jquery.inview.js" type="text/javascript"></script>
+<script type="text/javascript">
+		jQuery(document).ready(function($) {
+ 
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+});
+
+</script>
+
+<!--[if !IE]>-->
+<script type="text/javascript">
+$(function(){
+	/* main background image. moves against the direction of scroll*/
+	$('#start-page').scrollParallax({
+		'speed': -0.2
+	});
+
+	/* inner items, moves slightly faster than the background */
+
+	/* two additional samples inside item2, both moving with direction of scroll*/
+
+
+	/* speakers */
+
+	
+	/* left circles */
+	$('.lwc-1').scrollParallax({
+		'speed': -0.5
+	});
+  $('.lwc-2').scrollParallax({
+		'speed': 0.5
+	});
+	$('.lwc-3').scrollParallax({
+		'speed': -0.4
+	});
+  $('.lbc-1').scrollParallax({
+		'speed': 0.4
+	});	
+	$('.lbc-2').scrollParallax({
+		'speed': -0.4
+	});	
+  $('.lbc-3').scrollParallax({
+		'speed': -0.4
+	});	
+  $('.lbc-4').scrollParallax({
+		'speed': 0.3
+	});	
+	$('.lbc-5').scrollParallax({
+		'speed': -0.4
+	});	
+	$('.double-circle-1').scrollParallax({
+		'speed': 0.3
+	})
+  $('.double-circle-2').scrollParallax({
+		'speed': 0.5
+	});
+	
+	
+	/* right circles */
+	$('.rwc-1').scrollParallax({
+		'speed': -0.4
+	});
+	$('.rwc-3').scrollParallax({
+		'speed': 0.3
+	})
+	$('.rwc-4').scrollParallax({
+		'speed': -0.5
+	})
+	$('.rwc-5').scrollParallax({
+		'speed': -0.4
+	})
+	$('.rwc-6').scrollParallax({
+		'speed': 0.2
+	})
+	$('.rbc-1').scrollParallax({
+		'speed': 0.1
+	})
+	$('.rbc-2').scrollParallax({
+		'speed': 0.4
+	})
+	$('.rbc-3').scrollParallax({
+		'speed': -0.4
+	})
+	$('.sponsors-circle').scrollParallax({
+		'speed': 0
+	})
+	$('.blockquote-circle').scrollParallax({
+		'speed': -0.1
+	})
+	
+});
+</script>
+<!--<![endif]-->
+ 
+<script type="text/javascript">
+/*
+Firefox super responsive scroll (c) Keith Clark - MIT Licensed
+*/
+(function(doc) {
+
+  var root = doc.documentElement;
+
+  // Not ideal, but better than UA sniffing.
+  if ("MozAppearance" in root.style) {
+
+    // determine the vertical scrollbar width
+    var scrollbarWidth = root.clientWidth;
+    root.style.overflow = "scroll";
+    scrollbarWidth -= root.clientWidth;
+    root.style.overflow = "";
+
+    // create a synthetic scroll event
+    var scrollEvent = doc.createEvent("UIEvent")
+    scrollEvent.initEvent("scroll", true, true);
+
+    // event dispatcher
+    function scrollHandler() {
+      doc.dispatchEvent(scrollEvent)
+    }
+
+    // detect mouse events in the document scrollbar track
+    doc.addEventListener("mousedown", function(e) {
+      if (e.clientX > root.clientWidth - scrollbarWidth) {
+        doc.addEventListener("mousemove", scrollHandler, false);
+        doc.addEventListener("mouseup", function() {
+          doc.removeEventListener("mouseup", arguments.callee, false);
+          doc.removeEventListener("mousemove", scrollHandler, false);
+        }, false)
+      }
+    }, false)
+
+    // override mouse wheel behaviour.
+    doc.addEventListener("DOMMouseScroll", function(e) {
+      // Don't disable hot key behaviours
+      if (!e.ctrlKey && !e.shiftKey) {
+        root.scrollTop += e.detail * 16;
+        scrollHandler.call(this, e);
+        e.preventDefault()
+      }
+    }, false)
+
+  }
+})(document); 
+</script>
---
+++ index.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+  <head>
+	  <meta charset="utf-8" />
+	  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<title>Mind the Product | Product Management Conference | 2012</title>
+    <!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<![endif]-->
+		<meta name="description" content="MindtheProduct 2012 is the first conference of its kind - bringing together product management leaders from around the world to explore the intersection of design, technology and business - the business of building great products."/>
+	  <meta name="keywords" content="product management conference, product manager conference, product design conference, web conference, London, 2012, MindtheProduct, ProductTank, ProductCamp"/>
+	  <!-- stylesheets -->
+		<link rel="stylesheet" type="text/css" href="css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<!-- typekit -->
+		<script type="text/javascript" src="http://use.typekit.com/mus3wjm.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+		<?php include('includes/nav-script.inc.php'); ?>
+	</head>
+	
+	<body>
+	
+		<div id="global-container">
+		
+			<!-- START-PAGE -->
+			<div id="start-page">
+			
+				<div class="header" id="home">
+					<h1><a href="#" class="logo">Mind the Product | Product Management Conference | 2012</a></h1>
+					<div class="buy-tickets">
+						<a class="tab" href="#">Buy Tickets</a>
+						<div class="price">&pound;175 / ticket</div>
+					</div><!-- end buy-tickets -->
+					<div class="clear"></div>
+				</div><!-- end header -->
+				
+				<div class="content">
+					<ul class="main-nav">
+						<li><a class="selected scroll" href="#home">Home</a></li>
+						<li><a href="#speakers" class="scroll">Speakers</a></li>
+						<li><a href="#schedule" class="scroll">Schedule</a></li>
+						<li><a href="#location" class="scroll">Location</a></li>
+						<li><a href="#the-team" class="scroll">The Team</a></li>
+					</ul>
+					
+					<div class="indent">
+						<h2>The business of building great products</h2>
+						<p class="dark">On Friday, September 28th 2012<br /> at the Mermaid Conference Center in London</p>
+						<p class="intro">MindtheProduct 2012 is the first conference of its kind - bringing together product management leaders from around the world to explore the intersection of design, technology and business - the business of building great products.</p>
+					</div><!-- end indent -->
+				</div><!-- end content -->
+				
+				<!-- left background circles -->
+				<div class="lbc-1"></div>
+				<div class="lwc-1"></div>
+				
+				<!-- right background circles -->
+				<div class="rwc-1"></div>
+				<div class="rbc-1"></div>
+				<div class="rwc-2">
+					<div class="sponsors-small">
+						<p>Brought to you by</p>
+						<img src="images/the-guardian.png" width="230" height="41" alt="The Guardian" />
+						<img class="last" src="images/thoughtworks.png" width="221" height="35" alt="Thoughtworks" />
+					</div><!-- end sponsors-small -->
+				</div><!-- rwc-2 -->
+
+				<!-- ticket image -->
+				<div class="ticket"></div>
+			</div>
+			<!-- END START-PAGE -->
+			
+			<!-- SPEAKERS -->
+			<div id="speakers">
+				
+				<div class="top-bar">
+					<div class="content">
+						<a href="#home"><img class="logo-small" src="images/logo-small.png" width="98" height="30" alt="Mind the Product" /></a>
+						<p class="date">Friday, 28th September 2012
+						<a class="facebook" href="#"></a>
+						<a class="twitter" href="#"></a></p>
+						
+						<div class="buy-tickets">
+							<a class="tab" href="#">Buy Tickets</a>
+						</div><!-- end buy-tickets -->
+						
+						<h3>Speakers</h3>
+						<div class="clear"></div>
+					</div><!-- end content -->
+				</div><!-- end top-bar -->
+				
+				<div class="content">
+					<ul class="main-nav">
+						<li><a href="#home" class="scroll">Home</a></li>
+						<li><a class="selected scroll" href="#speakers">Speakers</a></li>
+						<li><a href="#schedule" class="scroll">Schedule</a></li>
+						<li><a href="#location" class="scroll">Location</a></li>
+						<li><a href="#the-team" class="scroll">The Team</a></li>
+					</ul><!-- end main-nav -->
+					
+					<div class="first speaker-right">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end first speaker-right -->
+					
+					<div class="first speaker-left">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end first speaker-left -->
+					
+					<div class="second speaker-right">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end second speaker-right -->
+					
+					<div class="second speaker-left">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end second speaker-left -->
+					
+					<div class="third speaker-right">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end third speaker-right -->
+					
+					<div class="third speaker-left">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end third speaker-left -->
+					
+					<div class="fourth speaker-right">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end fourth speaker-right -->
+					
+					<div class="fourth speaker-left">
+						<div class="white-bg">
+							<img src="images/speaker.png" width="152" height="152" alt="Speaker 1" />
+						</div>
+						<h4>Martin Erikkson</h4>
+						<p class="speciality">speciality goes here</p>
+						<p class="info">Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+						<a class="twitter" href="#">bfmartin</a>
+					</div><!-- end fourth speaker-left -->
+					<div class="clear"></div>
+				</div><!-- end content -->
+				
+				<!-- left background circles -->
+				<div class="lwc-2"></div>
+				<div class="lbc-2"></div>
+				
+				<!-- right background circles -->
+				<div class="rwc-3"></div>
+			</div>
+			<!-- END SPEAKERS -->
+			
+			<!-- SCHEDULE -->
+			<div id="schedule">
+				
+				<div class="top-bar">
+					<div class="content">
+						<a href="#home"><img class="logo-small" src="images/logo-small.png" width="98" height="30" alt="Mind the Product" /></a>
+						<p class="date">Friday, 28th September 2012
+						<a class="facebook" href="#"></a>
+						<a class="twitter" href="#"></a></p>
+						
+						<div class="buy-tickets">
+							<a class="tab" href="#">Buy Tickets</a>
+						</div><!-- end buy-tickets -->
+						
+						<h3>Schedule</h3>
+						<div class="clear"></div>
+					</div><!-- end content -->
+				</div><!-- end top-bar -->
+				
+				<div class="content">
+					<ul class="main-nav">
+						<li><a href="#home" class="scroll">Home</a></li>
+						<li><a href="#speakers" class="scroll">Speakers</a></li>
+						<li><a class="selected scroll" href="#schedule">Schedule</a></li>
+						<li><a href="#location" class="scroll">Location</a></li>
+						<li><a href="#the-team" class="scroll">The Team</a></li>
+					</ul><!-- end main-nav -->
+					
+					<table>
+						<tr>
+							<td>08.45</td>
+							<td>Registration</td>
+						</tr>
+						<tr>
+							<td>09.45</td>
+							<td>Intro</td>
+						</tr>
+						<tr>
+							<td>10.00</td>
+							<td>Speaker 1</td>
+						</tr>
+						<tr>
+							<td>10.45</td>
+							<td>Speaker 2</td>
+						</tr>
+						<tr>
+							<td>11.30</td>
+							<td>Break</td>
+						</tr>
+						<tr>
+							<td>12.00</td>
+							<td>Speaker 3</td>
+						</tr>
+						<tr>
+							<td>12.45</td>
+							<td>Speaker 4</td>
+						</tr>
+						<tr>
+							<td>13.30</td>
+							<td>Lunch (provided)</td>
+						</tr>
+						<tr>
+							<td>15.00</td>
+							<td>Speaker 5</td>
+						</tr>
+						<tr>
+							<td>15.45</td>
+							<td>Speaker 6</td>
+						</tr>
+						<tr>
+							<td>16.30</td>
+							<td>Break</td>
+						</tr>
+						<tr>
+							<td>17.00</td>
+							<td>Speaker 7</td>
+						</tr>
+						<tr>
+							<td>17.45</td>
+							<td>Speaker 8</td>
+						</tr>
+						<tr>
+							<td>18.15</td>
+							<td>Finish</td>
+						</tr>
+						<tr>
+							<td class="space">19.00</td>
+							<td class="space">After party</td>
+						</tr>
+					</table>
+					
+					<div class="clear"></div>
+				</div><!-- end content -->
+				
+				<!-- left background circles -->
+				<div class="double-circle-1"></div>
+				<div class="lbc-3"></div>
+				
+				<!-- right background circles -->
+				<div class="rwc-4"></div>
+				<div class="sponsors-circle">
+					<div class="sponsors-large">
+						<p>Brought to you by</p>
+						<div class="sponsor-large"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-large space"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium right"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium right"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium space right"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-medium space"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small middle"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small right"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small middle"><img src="#" width="#" height="#" alt="#" /></div>
+						<div class="sponsor-small right"><img src="#" width="#" height="#" alt="#" /></div>
+					</div><!-- end sponsors-large -->
+				</div><!-- end sponsors-circle -->
+				<div class="rbc-2"></div>
+				<div class="rwc-5"></div>
+			</div>
+			<!-- END SCHEDULE -->
+				
+			<!-- LOCATION -->
+			<div id="location">
+				
+				<div class="top-bar">
+					<div class="content">
+						<a href="#home"><img class="logo-small" src="images/logo-small.png" width="98" height="30" alt="Mind the Product" /></a>
+						<p class="date">Friday, 28th September 2012
+						<a class="facebook" href="#"></a>
+						<a class="twitter" href="#"></a></p>
+						
+						<div class="buy-tickets">
+							<a class="tab" href="#">Buy Tickets</a>
+						</div><!-- end buy-tickets -->
+						
+						<h3>Location</h3>
+						<div class="clear"></div>
+					</div><!-- end content -->
+				</div><!-- end top-bar -->
+				
+				<div class="content">
+					<ul class="main-nav">
+						<li><a href="#home" class="scroll">Home</a></li>
+						<li><a href="#speakers" class="scroll">Speakers</a></li>
+						<li><a href="#schedule" class="scroll">Schedule</a></li>
+						<li><a class="selected scroll" href="#location">Location</a></li>
+						<li><a href="#the-team" class="scroll">The Team</a></li>
+					</ul><!-- end main-nav -->
+				
+					<div class="map">
+						<img src="images/map.png" id="map-image" />
+						<div class="clear"></div>
+					</div><!-- end map -->
+					
+					<div class="map-overlay">
+						<div class="pointer"></div>
+						<div class="speech-bubble">
+							<h4>Mermaid<br /> Conference Center</h4>
+							<p class="address">Puddle Dock,<br /> London,<br /> Blackfriars<br /> EC4V 3DB</p>
+							<a class="get-directions" href="#">Get directions</a>
+						</div><!-- end speech-bubble -->
+					</div><!-- end map-overlay -->
+					
+					<img src="images/theatre.png" id="theatre" />
+				</div><!-- end content -->
+				
+				<!-- left background circles -->
+				<div class="lbc-4"></div>
+				<div class="lwc-3"></div>
+				
+				<!-- right background circles -->
+				<div class="rbc-3"></div>
+			</div>
+			<!-- END LOCATION -->
+			
+			<!-- THE TEAM -->
+			<div id="the-team">
+				
+				<div class="top-bar">
+					<div class="content">
+						<a href="#home"><img class="logo-small" src="images/logo-small.png" width="98" height="30" alt="Mind the Product" /></a>
+						<p class="date">Friday, 28th September 2012
+						<a class="facebook" href="#"></a>
+						<a class="twitter" href="#"></a></p>
+						
+						<div class="buy-tickets">
+							<a class="tab" href="#">Buy Tickets</a>
+						</div><!-- end buy-tickets -->
+						
+						<h3>The team</h3>
+						<div class="clear"></div>
+					</div><!-- end content -->
+				</div><!-- end top-bar -->
+				
+				<div class="content">
+					<ul class="main-nav">
+						<li><a href="#home" class="scroll">Home</a></li>
+						<li><a href="#speakers" class="scroll">Speakers</a></li>
+						<li><a href="#schedule" class="scroll">Schedule</a></li>
+						<li><a href="#location" class="scroll">Location</a></li>
+						<li><a class="selected scroll" href="#the-team">The Team</a></li>
+					</ul><!-- end main-nav -->
+					
+					<div class="indent">
+						<p class="intro">MindTheProduct 2012 is brought to you by a team of four product managers who spend all day building start-ups, and all night building a community  for other product managers.</p>
+						<p>We founded and run <a href="#">ProductTank</a>, a monthly product management meet up in London, which has been running for two years and now has over 850 members in the group and gets 120+ attendees at every meetup.</p>
+						<p>We founded and run <a href="#">ProductCamp London</a> bi-annually - a collaborative, user organized unconference based on the global ProductCamp.org.</p>
+						<p>To bring our events together we founded <a href="#">MindtheProduct.com</a>, a blog focused on product management, which we define as the intersection between design, technology and business. The blog combines our own editorial with community contributed posts.</p>
+						<p class="last">This site was designed by <a href="http://thisiscapra.com/"><img src="images/capra-logo.png" width="186" height="57" alt="Capra" /></a></p>
+						
+					</div><!-- end indent -->
+				</div><!-- end content -->
+				
+				<!-- left background circles -->
+				<div class="double-circle-2"></div>
+				<div class="lbc-5"></div>
+				
+				<!-- right background circles -->
+				<div class="blockquote-circle">
+					<blockquote>One of the best ways to learn about Product Management from peers, and for product managers, 
+					designers and marketers to socialise with like-minded people.</blockquote>
+				</div><!-- end blockquote-circle -->
+				<div class="rwc-6"></div>
+				<div class="team-image"><img src="images/the-team.png" /></div>
+			</div>
+			<!-- END THE TEAM -->
+			
+			<div id="footer">
+				<ul>
+					<li><a href="#" class="twitter">Follow us on twitter</a></li>
+					<li><a href="#" class="facebook">Like us on facebook</a></li>
+				</ul>
+			</div><!-- end footer -->		
+			
+		</div><!-- end global-container --> 
+	</body>
+</html>
